0955a591fd19e2ee26d004680e7ee7f65708ae67
John Merigliano <jmerigliano@gmail.com>
2021-11-23 21:09:07 -0800

    added DB schema

--- app.py
+++ app.py
@@ -4,9 +4,45 @@ from markupsafe import escape
 from flask import url_for
 from flask import request
 from flask import render_template
-
+import sqlite3
+from flask import g
 
 app = Flask(__name__)
+
+# ------------------------------
+# Database operations
+
+DATABASE = 'stocks.db'
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+# ------------------------------
+# Functions
+
+# calculators
+# and summary scripts
+
+
+# ------------------------------
+# Views
 
 # main index
 @app.route('/')
---
+++ schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS stocks_log;
+
+CREATE TABLE stocks_log (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    investment VARCHAR(10) NOT NULL,
+    date_entered TIMESTAMP NOT NULL,
+    shares INTEGER NOT NULL,
+    entry FLOAT NOT NULL,
+    stop FLOAT NOT NULL,
+    target FLOAT NOT NULL,
+    risk_share FLOAT NOT NULL,
+    exit FLOAT NOT NULL,
+    sell_date TIMESTAMP NOT NULL,
+    pnl FLOAT NOT NULL,
+    notes TEXT NOT NULL,
+    chart_url VARCHAR(255)
+);
